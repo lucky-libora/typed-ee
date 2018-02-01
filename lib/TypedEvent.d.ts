21a134b344bc5e02c78d9e6e73cbc69acfa6749f
Aleksey Vasilyev <luckylibora@gmail.com>
2018-02-02 01:35:31 +0300

    fix ts declaration

--- lib/TypedEvent.d.ts
+++ lib/TypedEvent.d.ts
@@ -3,8 +3,9 @@ export declare class TypedEvent<T> {
     name: string;
     private eventEmitter;
     constructor(name: string, eventEmitter: EventEmitter);
+    asyncEmit(arg: T): boolean;
     emit(arg: T): boolean;
-    off(cb: (T) => any): TypedEvent<T>;
-    on(cb: (T) => any): TypedEvent<T>;
-    once(cb: (T) => any): TypedEvent<T>;
+    off(cb: (arg: T) => any): TypedEvent<T>;
+    on(cb: (arg: T) => any): TypedEvent<T>;
+    once(cb: (arg: T) => any): TypedEvent<T>;
 }
